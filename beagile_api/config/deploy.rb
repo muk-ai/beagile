1bb73aaca20ba296962a222933ee98ac1e4193cb
Kenta Mukai <r@muk.ai>
2019-05-26 00:20:48 +0900

    :gear: add master.key to linked_files

--- beagile_api/config/deploy.rb
+++ beagile_api/config/deploy.rb
@@ -25,6 +25,7 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
+append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
