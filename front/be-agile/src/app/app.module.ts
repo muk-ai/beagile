71cd4983c16a05431c5a0be6c35ade120ea7b919
Kenta Mukai <r@muk.ai>
2019-08-29 11:52:57 +0000

    :gear: config firebase

--- front/be-agile/src/app/app.module.ts
+++ front/be-agile/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBE6-TKF0vPwC7B0OxXKrtVvNJZzf22nsQ",
+      authDomain: "beagile.firebaseapp.com",
+      databaseURL: "https://beagile.firebaseio.com",
+      projectId: "beagile",
+      storageBucket: "beagile.appspot.com",
+      messagingSenderId: "256420537000"
+    }),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
